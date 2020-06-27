3134966da88e05110f1a69f644a72bc4de87c897
Erick Alves <erick.elite.designer@gmail.com>
2020-06-27 21:04:25 +0100

    ìnitial

---
+++ server.lua
@@ -0,0 +1,42 @@
+-- Script feito por Erick Alves
+-- http://erick.pt
+-- http://github.com/erickcouto
+
+local Tunnel = module("vrp","lib/Tunnel")
+local Proxy = module("vrp","lib/Proxy")
+vRP = Proxy.getInterface("vRP")
+
+RegisterCommand("loteria",function(source,args)
+    
+    local maxValue = 50000
+	local source = source
+	local user_id = vRP.getUserId(source)
+	local banco = vRP.getBankMoney(user_id)
+
+		local random = math.random(2);
+		bankMoney = vRP.getBankMoney(user_id)
+        if parseInt(args[1]) <= maxValue then 
+            if banco >= parseInt(args[1]) then
+                if args[1] and parseInt(args[1]) > 0 then
+                    if (random % 2 == 0) then
+                        local value = parseInt(args[1])*2
+                        newBankMoney = parseInt(bankMoney + value)
+                        vRP.setBankMoney(user_id, newBankMoney)
+                        TriggerClientEvent("Notify",source,"sucesso","<b>Parabens!</b> Você ganhou! <b>R$"..vRP.format(value) .."</b>. Faça bom proveito!")
+                    else
+                        local value = args[1]
+                        newBankMoney = parseInt(bankMoney - value)
+                        vRP.setBankMoney(user_id, newBankMoney)
+                        TriggerClientEvent("Notify",source,"negado","<b>Não foi desta vez! </b> Você perdeu <b>R$"..vRP.format(args[1]).."</b>. Boa sorte na próxima vez.")
+                    end
+                else
+                    TriggerClientEvent("Notify",source,"importante","<b>Você precisa dizer quanto quer apostar")
+                end	
+            else
+                TriggerClientEvent("Notify",source,"importante","<b>Você não tem dinheiro suficiente no banco para fazer a aposta.")
+            end
+        else
+            TriggerClientEvent("Notify",source,"importante","<b>O valor máximo de apostas é R$"..vRP.format(maxValue))
+        end
+end)
+
